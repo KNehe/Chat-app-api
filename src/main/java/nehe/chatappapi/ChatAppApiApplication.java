b8dfc5cd99dab4ddcf39da08b6887193d500a236
= <kamolunehemiah@gmail.com>
2020-05-27 23:24:47 +0300

    first commit

---
+++ src/main/java/nehe/chatappapi/ChatAppApiApplication.java
@@ -0,0 +1,13 @@
+package nehe.chatappapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChatAppApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChatAppApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/nehe/chatappapi/ChatAppApiApplicationTests.java
@@ -0,0 +1,13 @@
+package nehe.chatappapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChatAppApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
